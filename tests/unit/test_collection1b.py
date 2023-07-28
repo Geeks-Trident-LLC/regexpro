0225fe3340543c4c51cc2477f6fb362d77d22101
Tuyen M. Duong <tuyen@geekstrident.com>
2023-07-28 14:54:48 -0700

    add more test cases for test_collection1b.py

--- tests/unit/test_collection1b.py
+++ tests/unit/test_collection1b.py
@@ -13,17 +13,39 @@ class TestElementPattern:
             ('word(var_v1, or_repeating_5_space)', '(?P<v1>( {5})|([a-zA-Z][a-zA-Z0-9]*))'),
             ('word(var_v1, or_repeat_5_spaces)', '(?P<v1>( {5})|([a-zA-Z][a-zA-Z0-9]*))'),
             ('word(var_v1, or_repeats_5_spaces)', '(?P<v1>( {5})|([a-zA-Z][a-zA-Z0-9]*))'),
-            ('word(var_v1, or_repeating_2_5_spaces)', '(?P<v1>( {2,5})|([a-zA-Z][a-zA-Z0-9]*))'),
-            ('word(var_v1, or_repeating__5_spaces)', '(?P<v1>( {,5})|([a-zA-Z][a-zA-Z0-9]*))'),
-            ('word(var_v1, or_at_least_5_spaces)', '(?P<v1>( {5,})|([a-zA-Z][a-zA-Z0-9]*))'),
-            ('word(var_v1, or_at_most_5_spaces)', '(?P<v1>( {,5})|([a-zA-Z][a-zA-Z0-9]*))'),
             ('word(var_v1, or_5_spaces)', '(?P<v1>( {5})|([a-zA-Z][a-zA-Z0-9]*))'),
-            ('word(var_v1, or_at_least_5_occurrences_spaces)', '(?P<v1>( {5,})|([a-zA-Z][a-zA-Z0-9]*))'),
-            ('word(var_v1, or_at_most_5_occurrences_spaces)', '(?P<v1>( {,5})|([a-zA-Z][a-zA-Z0-9]*))'),
             ('word(var_v1, or_5_occurrences_spaces)', '(?P<v1>( {5})|([a-zA-Z][a-zA-Z0-9]*))'),
+
+            ('word(var_v1, or_repeating_2_5_spaces)', '(?P<v1>( {2,5})|([a-zA-Z][a-zA-Z0-9]*))'),
+            ('word(var_v1, or_repeat_2_5_spaces)', '(?P<v1>( {2,5})|([a-zA-Z][a-zA-Z0-9]*))'),
+            ('word(var_v1, or_repeats_2_5_spaces)', '(?P<v1>( {2,5})|([a-zA-Z][a-zA-Z0-9]*))'),
+
+            ('word(var_v1, or_repeating__5_spaces)', '(?P<v1>( {,5})|([a-zA-Z][a-zA-Z0-9]*))'),
+            ('word(var_v1, or_repeat__5_spaces)', '(?P<v1>( {,5})|([a-zA-Z][a-zA-Z0-9]*))'),
+            ('word(var_v1, or_repeats__5_spaces)', '(?P<v1>( {,5})|([a-zA-Z][a-zA-Z0-9]*))'),
+            ('word(var_v1, or_at_most_5_spaces)', '(?P<v1>( {,5})|([a-zA-Z][a-zA-Z0-9]*))'),
+            ('word(var_v1, or_at_most_5_occurrences_spaces)', '(?P<v1>( {,5})|([a-zA-Z][a-zA-Z0-9]*))'),
+
+            ('word(var_v1, or_repeat_5__spaces)', '(?P<v1>( {5,})|([a-zA-Z][a-zA-Z0-9]*))'),
+            ('word(var_v1, or_at_least_5_spaces)', '(?P<v1>( {5,})|([a-zA-Z][a-zA-Z0-9]*))'),
+            ('word(var_v1, or_at_least_5_occurrences_spaces)', '(?P<v1>( {5,})|([a-zA-Z][a-zA-Z0-9]*))'),
+
+            ('word(var_v1, or_either_5_spaces)', '(?P<v1>( {5})|( *[a-zA-Z][a-zA-Z0-9]* *))'),
+            ('word(var_v1, or_either_repeat_5_spaces)', '(?P<v1>( {5})|( *[a-zA-Z][a-zA-Z0-9]* *))'),
+
             ('word(var_v1, or_either_repeating_2_5_spaces)', '(?P<v1>( {2,5})|( *[a-zA-Z][a-zA-Z0-9]* *))'),
+            ('word(var_v1, or_either_repeat_2_5_spaces)', '(?P<v1>( {2,5})|( *[a-zA-Z][a-zA-Z0-9]* *))'),
+            ('word(var_v1, or_either_repeating_2_5_spaces)', '(?P<v1>( {2,5})|( *[a-zA-Z][a-zA-Z0-9]* *))'),
+
+            ('word(var_v1, or_either_repeat_5__spaces)', '(?P<v1>( {5,})|( *[a-zA-Z][a-zA-Z0-9]* *))'),
             ('word(var_v1, or_either_at_least_5_occurrences_spaces)', '(?P<v1>( {5,})|( *[a-zA-Z][a-zA-Z0-9]* *))'),
+            ('word(var_v1, or_either_at_least_5_spaces)', '(?P<v1>( {5,})|( *[a-zA-Z][a-zA-Z0-9]* *))'),
+
+            ('word(var_v1, or_either_repeat__5_spaces)', '(?P<v1>( {,5})|( *[a-zA-Z][a-zA-Z0-9]* *))'),
+            ('word(var_v1, or_either_at_most_5_spaces)', '(?P<v1>( {,5})|( *[a-zA-Z][a-zA-Z0-9]* *))'),
             ('word(var_v1, or_either_at_most_5_occurrences_spaces)', '(?P<v1>( {,5})|( *[a-zA-Z][a-zA-Z0-9]* *))'),
+
+            ('word(var_v1, or_either_5_occurrences_spaces)', '(?P<v1>( {5})|( *[a-zA-Z][a-zA-Z0-9]* *))'),
         ]
     )
     def test_element_pattern(self, data, expected_result):
