f402b595294f641aad6f6927610b48b128052015
Tuyen M. Duong <tuyen@geekstrident.com>
2023-08-25 13:53:44 -0700

    fix typo mixed_word_group

---
+++ tests/unit/test_collection1f.py
@@ -0,0 +1,21 @@
+import pytest       # noqa
+import re
+from regexpro import ElementPattern
+
+
+class TestElementPatternF:
+
+    @pytest.mark.parametrize(
+        (
+            'data', 'expected_pattern',
+        ),
+        [
+            (
+                'mixed_word_group(var_meats)',
+                r'(?P<meats>[\x21-\x7e]*[a-zA-Z0-9][\x21-\x7e]*( +[\x21-\x7e]*[a-zA-Z0-9][\x21-\x7e]*)+)',
+            ),
+        ]
+    )
+    def test_element_pattern(self, data, expected_pattern):
+        pattern = ElementPattern(data)
+        assert pattern == expected_pattern
