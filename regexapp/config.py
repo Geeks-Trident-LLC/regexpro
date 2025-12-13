448c4f0a53a7a12d49b866ae615f02ce28eca23a
Tuyen M. Duong <tuyen@geekstrident.com>
2025-12-12 19:31:55 -0800

    chore: load license content from LICENSE file

--- regexapp/config.py
+++ regexapp/config.py
@@ -11,7 +11,7 @@ import yaml
 from genericlib import version as gtlib_version
 from genericlib import File
 
-__version__ = '0.5.0a0'
+__version__ = '0.5.1a1'
 version = __version__
 __edition__ = 'Community'
 edition = __edition__
@@ -62,7 +62,7 @@ class Data:
     pyyaml_link = 'https://pypi.org/project/PyYAML/'
 
     gtgenlib_text = f"genericlib v{gtlib_version}"
-    gtgenlib_link = ""
+    gtgenlib_link = "https://pypi.org/project/genericlib/"
 
     # company
     company = 'Geeks Trident LLC'
@@ -79,38 +79,8 @@ class Data:
     years = '2022'
     license_name = f'{company_name} License'
     copyright_text = f'Copyright \xa9 {years}'
-    license = dedent(
-        """
-        BSD 3-Clause License
-        
-        Copyright (c) 2021-2040, Geeks Trident LLC
-        All rights reserved.
-        
-        Redistribution and use in source and binary forms, with or without
-        modification, are permitted provided that the following conditions are met:
-        
-        1. Redistributions of source code must retain the above copyright notice, this
-           list of conditions and the following disclaimer.
-        
-        2. Redistributions in binary form must reproduce the above copyright notice,
-           this list of conditions and the following disclaimer in the documentation
-           and/or other materials provided with the distribution.
-        
-        3. Neither the name of the copyright holder nor the names of its
-           contributors may be used to endorse or promote products derived from
-           this software without specific prior written permission.
-        
-        THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-        AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-        IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-        DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-        FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-        DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-        SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-        CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-        OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-        OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-        """).strip()    # noqa
+    with open("LICENSE", encoding="utf-8") as f:
+        license = f.read()
 
     @classmethod
     def get_dependency(cls):
